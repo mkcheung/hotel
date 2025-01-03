51a9a4c643a3514f943273cfa485c1499051053f
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-01-03 10:52:02 -0800

    implement user profile page

--- resources/views/frontend/dashboard/edit_profile.blade.php
+++ resources/views/frontend/dashboard/edit_profile.blade.php
@@ -1,6 +1,7 @@
 @extends('frontend.main_master')
 @section('main')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
   <!-- Inner Banner -->
   <div class="inner-banner inner-bg6">
@@ -32,68 +33,60 @@
 
             <div class="col-lg-9">
                 <div class="service-article">
-
+                    
 
     <section class="checkout-area pb-70">
     <div class="container">
-        <form>
+        <form action="{{ route('profile.store') }}" method="post" enctype="multipart/form-data">
+        @csrf
             <div class="row">
                 <div class="col-lg-12 col-md-12">
                     <div class="billing-details">
                         <h3 class="title">User Profile   </h3>
 
                         <div class="row">
+                           
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group"><input type="text" name="address" class="form-control" value="{{ $profileData->address }}">
+                <label>Name <span class="required">*</span></label>
+                <input type="text" name="name"  class="form-control" value="{{ $profileData->name }}">
+            </div>
+        </div>
 
-                            <div class="col-lg-6 col-md-6">
-                                <div class="form-group">
-                                    <label>First Name <span class="required">*</span></label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>Email <span class="required">*</span></label>
+                <input type="email" name="email" class="form-control" value="{{ $profileData->email }}">
+            </div>
+        </div>
 
-                            <div class="col-lg-6 col-md-6">
-                                <div class="form-group">
-                                    <label>Last Name <span class="required">*</span></label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
+         
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label> Address <span class="required">*</span></label>
+                <input type="text" name="address" class="form-control" value="{{ $profileData->address }}">            </div>
+        </div>
 
-                            <div class="col-lg-12 col-md-12">
-                                <div class="form-group">
-                                    <label>Company Name</label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-
-
-
-                            <div class="col-lg-6 col-md-6">
-                                <div class="form-group">
-                                    <label>Email Address <span class="required">*</span></label>
-                                    <input type="email" class="form-control">
-                                </div>
-                            </div>
-
-                            <div class="col-lg-6 col-md-6">
-                                <div class="form-group">
-                                    <label>Phone <span class="required">*</span></label>
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
+        <div class="col-lg-6 col-md-6">
+            <div class="form-group">
+                <label>Phone <span class="required">*</span></label>
+                <input type="text" name="phone" class="form-control" value="{{ $profileData->phone }}">
+            </div>
+        </div>
 
 
 
 <div class="col-lg-12 col-md-6">
 <div class="form-group">
     <label>User Profile  <span class="required">*</span></label>
-    <input type="file" class="form-control">
+    <input type="file" name="photo" class="form-control"  id="image">
 </div>
 </div>
 
 <div class="col-lg-12 col-md-6">
 <div class="form-group">
-    <label>Town / City <span class="required">*</span></label>
-    <input type="text" class="form-control">
+    <label>  <span class="required"> </span></label>
+    <img id="showImage" src="{{ (!empty($profileData->photo)) ? url('upload/user_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
 </div>
 </div>
 
@@ -103,21 +96,33 @@
 </div>
 </div>
 </form>      
-
+        
     </div>
 </section>
-
+                    
                 </div>
             </div>
 
-
+           
         </div>
     </div>
 </div>
 <!-- Service Details Area End -->
 
 
+<script type="text/javascript">
 
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+
+    </script>      
 
 
 
--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -4,37 +4,41 @@
         <!-- Required Meta Tags -->
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+ 
         <!-- Bootstrap CSS --> 
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/bootstrap.min.css') }}">
         <!-- Animate Min CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/animate.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/animate.min.css') }}">
         <!-- Flaticon CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/fonts/flaticon.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/fonts/flaticon.css') }}">
         <!-- Boxicons CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/boxicons.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/boxicons.min.css') }}">
         <!-- Magnific Popup CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/magnific-popup.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/magnific-popup.css') }}">
         <!-- Owl Carousel Min CSS --> 
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/owl.carousel.min.css')}}">
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/owl.theme.default.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/owl.carousel.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/owl.theme.default.min.css') }}">
         <!-- Nice Select Min CSS --> 
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/nice-select.min.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/nice-select.min.css') }}">
         <!-- Meanmenu CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/meanmenu.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/meanmenu.css') }}">
         <!-- Jquery Ui CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/jquery-ui.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/jquery-ui.css') }}">
         <!-- Style CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/style.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/style.css') }}">
         <!-- Responsive CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/responsive.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/responsive.css') }}">
         <!-- Theme Dark CSS -->
-        <link rel="stylesheet" href="{{asset('frontend/assets/css/theme-dark.css')}}">
+        <link rel="stylesheet" href="{{ asset('frontend/assets/css/theme-dark.css') }}">
 
         <!-- Favicon -->
-        <link rel="icon" type="image/png" href="{{asset('frontend/assets/img/favicon.png')}}">
+        <link rel="icon" type="image/png" href="{{ asset('frontend/assets/img/favicon.png') }}">
 
-        <title>Easy Hotel</title>
+        	<!-- toastr CSS -->
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+    <!-- toastr CSS -->
+
+        <title>Easy Hotel </title>
     </head>
     <body>
 
@@ -54,44 +58,70 @@
         <!-- PreLoader End -->
 
         <!-- Top Header Start -->
-         @include('frontend.body.header')
+        @include('frontend.body.header')
         <!-- Top Header End -->
 
         <!-- Start Navbar Area -->
-         @include('frontend.body.navbar')
+        @include('frontend.body.navbar')
         <!-- End Navbar Area -->
 
-       @yield('main')
+        @yield('main')
 
         <!-- Footer Area -->
-         @include('frontend.body.footer')
+        @include('frontend.body.footer')
         <!-- Footer Area End -->
 
 
         <!-- Jquery Min JS -->
-        <script src="{{asset('frontend/assets/js/jquery.min.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/jquery.min.js') }}"></script>
         <!-- Bootstrap Bundle Min JS -->
-        <script src="{{asset('frontend/assets/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/bootstrap.bundle.min.js') }}"></script>
         <!-- Magnific Popup Min JS -->
-        <script src="{{asset('frontend/assets/js/jquery.magnific-popup.min.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/jquery.magnific-popup.min.js') }}"></script>
         <!-- Owl Carousel Min JS -->
-        <script src="{{asset('frontend/assets/js/owl.carousel.min.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/owl.carousel.min.js') }}"></script>
         <!-- Nice Select Min JS -->
-        <script src="{{asset('frontend/assets/js/jquery.nice-select.min.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/jquery.nice-select.min.js') }}"></script>
         <!-- Wow Min JS -->
-        <script src="{{asset('frontend/assets/js/wow.min.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/wow.min.js') }}"></script>
         <!-- Jquery Ui JS -->
-        <script src="{{asset('frontend/assets/js/jquery-ui.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/jquery-ui.js') }}"></script>
         <!-- Meanmenu JS -->
-        <script src="{{asset('frontend/assets/js/meanmenu.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/meanmenu.js') }}"></script>
         <!-- Ajaxchimp Min JS -->
-        <script src="{{asset('frontend/assets/js/jquery.ajaxchimp.min.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/jquery.ajaxchimp.min.js') }}"></script>
         <!-- Form Validator Min JS -->
-        <script src="{{asset('frontend/assets/js/form-validator.min.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/form-validator.min.js') }}"></script>
         <!-- Contact Form JS -->
-        <script src="{{asset('frontend/assets/js/contact-form-script.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/contact-form-script.js') }}"></script>
         <!-- Custom JS -->
-        <script src="{{asset('frontend/assets/js/custom.js')}}"></script>
+        <script src="{{ asset('frontend/assets/js/custom.js') }}"></script>
         
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<script>
+ @if(Session::has('message'))
+ var type = "{{ Session::get('alert-type','info') }}"
+ switch(type){
+    case 'info':
+    toastr.info(" {{ Session::get('message') }} ");
+    break;
+
+    case 'success':
+    toastr.success(" {{ Session::get('message') }} ");
+    break;
+
+    case 'warning':
+    toastr.warning(" {{ Session::get('message') }} ");
+    break;
+
+    case 'error':
+    toastr.error(" {{ Session::get('message') }} ");
+    break; 
+ }
+ @endif 
+</script>
+
+
     </body>
 </html>
