8f8f3545903c7f4021aeb2d99a32185e7b14692a
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-01-03 14:27:49 -0800

    establish Team Controller

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Backend\TeamController;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,3 +52,11 @@ Route::middleware(['auth', 'roles:admin'])->group(function(){
 }); // End Admin Group Middleware
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+
+Route::middleware(['auth', 'roles:admin'])->group(function(){
+
+    Route::controller(TeamController::class)->group(function(){
+        Route::get('/all/team', 'AllTeam')->name('all.team');
+    });
+}); // End Admin Group Middleware
