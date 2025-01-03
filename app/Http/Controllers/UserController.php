285ba49fe7c3826a5be33c5d895e2d9b268beaa4
Mars Cheung <mars.kwong.cheung@gmail.com>
2025-01-03 13:19:34 -0800

    implement logout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,4 +48,19 @@ class UserController extends Controller
         return redirect()->back()->with($notification);
 
     }// End Method 
+    public function UserLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        $notification = array(
+            'message' => 'User Logout Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
+    }
 }
